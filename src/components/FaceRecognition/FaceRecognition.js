bd4991c3fd060e110e33897baea848666bbe5f38
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-04-07 23:15:26 -0400

    New Component for Image display

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,3 +1,17 @@
 import React from "react";
+import "./FaceRecognition.css"
+
+const FaceRecognition = ({box, inputURL}) => {
+    return (
+        <div className='center ma'>
+            <div className='absolute mt2'>
+            <img id = "imageToAdjust" alt= 'Basic' src={inputURL} width="800px" heigh="auto" />
+            <div className='boxBoundaries' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow , left: box.leftCol }}></div>
+            </div>
+        </div>
+
+    )
+
+}
 
 export default FaceRecognition;
