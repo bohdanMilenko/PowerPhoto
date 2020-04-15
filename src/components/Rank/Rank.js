698a49d1f5b6a9ef948ec3f9b7bd4ff08ce60124
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-04-15 18:56:00 -0400

    Changed paragraphs layout

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,10 +4,10 @@ const Rank = () => {
     return (
         <div>
             <div className='white f3'>
-                {'User, your current rank is.. '}
+                {''}
             </div>
             <div className='white f3'>
-                {'#5'}
+                {''}
             </div>
         </div>
     )
