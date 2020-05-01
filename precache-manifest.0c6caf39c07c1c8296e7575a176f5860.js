a45f5a44d466e7c21797c6f9f557bc4d02a16646
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-05-01 11:27:15 -0400

    Updates

---
+++ precache-manifest.0c6caf39c07c1c8296e7575a176f5860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7235fd761a5e66afdee43a84b7079e3",
+    "url": "/PowerPhoto/index.html"
+  },
+  {
+    "revision": "0280fe6d968d8b56fb2a",
+    "url": "/PowerPhoto/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "8289569908fad0953afb",
+    "url": "/PowerPhoto/static/css/main.301a4025.chunk.css"
+  },
+  {
+    "revision": "0280fe6d968d8b56fb2a",
+    "url": "/PowerPhoto/static/js/2.24eba210.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PowerPhoto/static/js/2.24eba210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8289569908fad0953afb",
+    "url": "/PowerPhoto/static/js/main.a1dadc9c.chunk.js"
+  },
+  {
+    "revision": "072f48b3bd49bfbf4bce",
+    "url": "/PowerPhoto/static/js/runtime-main.8e94f07b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bohdanMilenko/PowerPhoto/precache-manifest.aeda0d2808f89e7f18259d2dfbd5f89b.js"
+  "/PowerPhoto/precache-manifest.0c6caf39c07c1c8296e7575a176f5860.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bohdanMilenko/PowerPhoto/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PowerPhoto/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
